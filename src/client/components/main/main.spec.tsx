5d1757cb92ecec614c7d38f40e197951b61f0980
guilleharriague <guillermo.harriague@distillery.com>
2021-10-14 15:42:44 -0300

    fix main tests

--- src/client/components/main/main.spec.tsx
+++ src/client/components/main/main.spec.tsx
@@ -3,16 +3,10 @@ import { render, fireEvent, waitFor, RenderResult } from '../../testUtils';
 import '@testing-library/jest-dom/extend-expect';
 
 import Main from './main';
-import { State, ToDoData } from '../../types';
+import { State } from '../../types';
 
 import toDoListInitialState from '../../store/slices/toDoList/initialState';
 import addToDoModalInitialState from '../../store/slices/addToDoModal/initialState';
-
-import ApiService from '../../services/apiService';
-
-jest.mock('../../services/apiService');
-
-const ApiServiceMock = ApiService as jest.MockedClass<typeof ApiService>;
 
 const basicState: State = {
   toDoList: toDoListInitialState,
@@ -41,50 +35,41 @@ describe('<Main />', () => {
     expect(getByText(/title/i)).toBeInTheDocument();
   });
 
-  test('when new element is added, it s rendered in the "To Do" section', async () => {
-    const mockResponse: ToDoData[] = [
-      {
-        id: '123abc',
-        title: 'Test ToDo',
-        isDone: false,
+  test('when there is an undone todo, it s rendered in the "To Do" section', () => {
+    const newState = {
+      toDoList: {
+        ...basicState.toDoList,
+        data: [
+          {
+            id: '123abc',
+            title: 'Test ToDo',
+            isDone: false,
+          },
+        ],
       },
-    ];
-    ApiServiceMock.getToDoList.mockResolvedValueOnce({ data: mockResponse });
+    };
 
-    const { getByRole, getByTestId } = renderMain();
+    const { getByTestId } = renderMain(newState);
 
-    fireEvent.click(getByRole('button', { name: /add todo/i }));
-    fireEvent.change(getByRole('textbox'), { target: { value: 'Test ToDo' } });
-    fireEvent.click(getByRole('button', {name: /add/i }));
-
-    await waitFor(() => {
-      expect(getByTestId('todo-content').children).toHaveLength(1);
-    });
+    expect(getByTestId('todo-content').children).toHaveLength(1);
   });
 
-  test('when an element is marked as done, it passes to the "Done" section', async () => {
-    const mockResponse: ToDoData[] = [
-      {
-        id: '123abc',
-        title: 'Test ToDo',
-        isDone: true,
+  test('when an element is marked as done, it passes to the "Done" section', () => {
+    const newState = {
+      toDoList: {
+        ...basicState.toDoList,
+        data: [
+          {
+            id: '123abc',
+            title: 'Test ToDo',
+            isDone: true,
+          },
+        ],
       },
-    ];
-    ApiServiceMock.toggleToDo.mockResolvedValueOnce({ data: mockResponse });
+    };
 
-    const { getByRole, getByTestId, getByText } = renderMain();
+    const { getByTestId } = renderMain(newState);
 
-    fireEvent.click(getByRole('button', { name: /add todo/i }));
-    fireEvent.change(getByRole('textbox'), { target: { value: 'Test ToDo' } });
-    fireEvent.click(getByRole('button', {name: /add/i }));
-
-    await waitFor(() => expect(getByText('Test ToDo')).toBeInTheDocument());
-
-    fireEvent.click(getByRole('checkbox', { name: /done/i }));
-
-    await waitFor(() => {
-      expect(getByTestId('todo-content').children).toHaveLength(0);
-      expect(getByTestId('done-content').children).toHaveLength(1);
-    });
+    expect(getByTestId('done-content').children).toHaveLength(1);
   });
 });
